81505329b07d928c5720c1045cd96b094cd7fce8
Матвей <vvhh3144@gmail.com>
2026-06-19 14:20:22 +0400

    test reacr bits

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,22 @@ import { Toaster } from "react-hot-toast"
 import Game from "./components/Game"
 
 //npm i @dnd-kit/core
+import Aurora from './components/Aurora';
+
 function App() {
 
 
   return (
     <>
+      <Aurora
+        colorStops={["#0061fc", "#8507f3", "#5227FF"]}
+        blend={0.37}
+        amplitude={1.0}
+        speed={0.9} />
       <Toaster
         position="top-center"
         reverseOrder={false} />
-        
+
       <Game />
     </>
   )
